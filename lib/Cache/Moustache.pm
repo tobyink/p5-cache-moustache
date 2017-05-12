44062e69fd7a55c1c9caf42119db5639fc044531
Toby Inkster <mail@tobyinkster.co.uk>
2017-05-12 09:33:59 +0100

    bump version; changelog

--- lib/Cache/Moustache.pm
+++ lib/Cache/Moustache.pm
@@ -8,7 +8,7 @@ use warnings;
 
 BEGIN {
 	$Cache::Moustache::AUTHORITY = 'cpan:TOBYINK';
-	$Cache::Moustache::VERSION   = '0.005';
+	$Cache::Moustache::VERSION   = '0.006';
 }
 
 sub isa
@@ -310,7 +310,7 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2012, 2014 by Toby Inkster.
+This software is copyright (c) 2012, 2014, 2017 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
